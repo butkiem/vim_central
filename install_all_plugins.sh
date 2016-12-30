df17c00e880e8db18c6b74b04e2b1735d200d001
butkiem <mariusz.butkiewicz@gmail.com>
2016-12-29 23:27:33 -0500

    cleanup

--- install_all_plugins.sh
+++ install_all_plugins.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+cd ~
+ln -s ~/vim_github ~/.vim
+
 # install pathogen (plugin manager)
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cd ~/.vim/bundle && git clone https://github.com/tpope/vim-sensible.git
+mkdir -p ~/vim_github/autoload ~/vim_github/bundle && curl -LSso ~/vim_github/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/vim_github/bundle && git clone https://github.com/tpope/vim-sensible.git
 
 #install plugins
 git submodule add git://github.com/ervandew/supertab.git bundle/supertab
 git submodule add git://github.com/terryma/vim-multiple-cursors.git bundle/vim-multiple-cursors
 git submodule add git://github.com/itchyny/lightline.vim.git bundle/lightline.vim
 git submodule add git://github.com/altercation/vim-colors-solarized.git bundle/vim-colors-solarized
+git submodule add git://github.com/tomasr/molokai.git bundle/vim-colors-molokai
+
+git submodule init
+git submodule update
